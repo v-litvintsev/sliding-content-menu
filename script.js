596e68d31d17fdbd255fe56d40740aa404c8d864
Vladimir Litvintsev <59085853+v-litvintsev@users.noreply.github.com>
2023-03-28 23:47:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -18,101 +18,116 @@ gsap.fromTo(
   }
 );
 
+const handleMenuOpen = () => {
+  menuList.classList.add("open");
+  gsap.set(document.body, { overflow: "hidden" });
+  gsap.set(content, { willChange: "transform, clip-path" });
+  gsap.set(menuToggle, {
+    top: window.scrollY,
+  });
+
+  requestIdleCallback(
+    () => {
+      gsap.fromTo(
+        menuList,
+        { height: 0 },
+        { height: "auto", duration: 1, ease: "power3.inOut" }
+      );
+
+      gsap.set(content, {
+        transformOrigin: `left ${window.scrollY + window.innerHeight / 2}px`,
+      });
+
+      gsap.to(content, {
+        x: "40vw",
+        scale: 0.9,
+        duration: 1,
+        ease: "power3.inOut",
+      });
+
+      gsap.fromTo(
+        content,
+        {
+          clipPath: `inset(${window.scrollY}px 0 ${
+            content.scrollHeight - (window.scrollY + window.innerHeight)
+          }px round 0px)`,
+        },
+        {
+          duration: 1,
+          ease: "power3.inOut",
+          clipPath: `inset(${window.scrollY}px 0 ${
+            content.scrollHeight - (window.scrollY + window.innerHeight)
+          }px round 30px)`,
+          onStart: () => {
+            menuToggle.classList.add("do-not-handle-events");
+          },
+          onComplete: () => {
+            menuToggle.classList.remove("do-not-handle-events");
+          },
+        }
+      );
+    },
+    { timeout: 100 }
+  );
+};
+
+const handleMenuClose = () => {
+  gsap.set(content, { willChange: "auto" });
+
+  gsap.to(menuList, {
+    height: 0,
+    duration: 1,
+    ease: "power3.inOut",
+    onComplete: function () {
+      menuList.classList.remove("open");
+      content.style.transform = "none";
+    },
+  });
+
+  gsap.to(content, {
+    x: 0,
+    y: 0,
+    scale: 1,
+    duration: 1,
+    ease: "power3.inOut",
+    onComplete: () => {
+      content.style.transform = "none";
+      gsap.set(document.body, { overflow: "visible" });
+      gsap.set(menuToggle, {
+        top: "auto",
+      });
+    },
+  });
+
+  gsap.fromTo(
+    content,
+    {
+      clipPath: `inset(${window.scrollY}px 0 ${
+        content.scrollHeight - (window.scrollY + window.innerHeight)
+      }px round 30px)`,
+    },
+    {
+      duration: 1,
+      ease: "power3.inOut",
+      clipPath: `inset(${window.scrollY}px 0 ${
+        content.scrollHeight - (window.scrollY + window.innerHeight)
+      }px round 0px)`,
+      onStart: () => {
+        menuToggle.classList.add("do-not-handle-events");
+      },
+      onComplete: () => {
+        gsap.set(content, { clipPath: "none" });
+        menuToggle.classList.remove("do-not-handle-events");
+      },
+    }
+  );
+};
+
 menuToggle.addEventListener("click", function () {
   if (menuList.classList.contains("open")) {
-    gsap.to(menuList, {
-      height: 0,
-      duration: 1,
-      ease: "power3.inOut",
-      onComplete: function () {
-        menuList.classList.remove("open");
-        content.style.transform = "none";
-      },
-    });
-
-    gsap.to(content, {
-      x: 0,
-      y: 0,
-      scale: 1,
-      duration: 1,
-      ease: "power3.inOut",
-      onComplete: () => {
-        content.style.transform = "none";
-        gsap.set(document.body, { overflow: "visible" });
-        gsap.set(menuToggle, {
-          top: "auto",
-        });
-      },
-    });
-
-    gsap.fromTo(
-      content,
-      {
-        clipPath: `inset(${window.scrollY}px 0 ${
-          content.scrollHeight - (window.scrollY + window.innerHeight)
-        }px round 30px)`,
-      },
-      {
-        duration: 1,
-        ease: "power3.inOut",
-        clipPath: `inset(${window.scrollY}px 0 ${
-          content.scrollHeight - (window.scrollY + window.innerHeight)
-        }px round 0px)`,
-        onStart: () => {
-          menuToggle.classList.add("do-not-handle-events");
-        },
-        onComplete: () => {
-          gsap.set(content, { clipPath: "none" });
-          menuToggle.classList.remove("do-not-handle-events");
-        },
-      }
-    );
+    handleMenuClose();
   } else {
-    menuList.classList.add("open");
-    gsap.set(document.body, { overflow: "hidden" });
-
-    gsap.fromTo(
-      menuList,
-      { height: 0 },
-      { height: "auto", duration: 1, ease: "power3.inOut" }
-    );
-
-    gsap.set(content, {
-      transformOrigin: `left ${window.scrollY + window.innerHeight / 2}px`,
-    });
-
-    gsap.to(content, {
-      x: "40vw",
-      scale: 0.9,
-      duration: 1,
-      ease: "power3.inOut",
-    });
-
-    gsap.fromTo(
-      content,
-      {
-        clipPath: `inset(${window.scrollY}px 0 ${
-          content.scrollHeight - (window.scrollY + window.innerHeight)
-        }px round 0px)`,
-      },
-      {
-        duration: 1,
-        ease: "power3.inOut",
-        clipPath: `inset(${window.scrollY}px 0 ${
-          content.scrollHeight - (window.scrollY + window.innerHeight)
-        }px round 30px)`,
-        onStart: () => {
-          menuToggle.classList.add("do-not-handle-events");
-        },
-        onComplete: () => {
-          menuToggle.classList.remove("do-not-handle-events");
-        },
-      }
-    );
-
-    gsap.set(menuToggle, {
-      top: window.scrollY,
-    });
+    handleMenuOpen();
   }
 });
 
@@ -138,52 +153,6 @@ menuItems.forEach(function (item) {
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
-    gsap.to(menuList, {
-      height: 0,
-      duration: 1,
-      ease: "power3.inOut",
-      onComplete: function () {
-        menuList.classList.remove("open");
-        content.style.transform = "none";
-      },
-    });
-
-    gsap.to(content, {
-      x: 0,
-      y: 0,
-      scale: 1,
-      duration: 1,
-      ease: "power3.inOut",
-      onComplete: () => {
-        content.style.transform = "none";
-        gsap.set(document.body, { overflow: "visible" });
-        gsap.set(menuToggle, {
-          top: "auto",
-        });
-      },
-    });
-
-    gsap.fromTo(
-      content,
-      {
-        clipPath: `inset(${window.scrollY}px 0 ${
-          content.scrollHeight - (window.scrollY + window.innerHeight)
-        }px round 30px)`,
-      },
-      {
-        duration: 1,
-        ease: "power3.inOut",
-        clipPath: `inset(${window.scrollY}px 0 ${
-          content.scrollHeight - (window.scrollY + window.innerHeight)
-        }px round 0px)`,
-        onStart: () => {
-          menuToggle.classList.add("do-not-handle-events");
-        },
-        onComplete: () => {
-          gsap.set(content, { clipPath: "none" });
-          menuToggle.classList.remove("do-not-handle-events");
-        },
-      }
-    );
+    handleMenuClose();
   });
 });
